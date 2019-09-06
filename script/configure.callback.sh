85dd41fb7bd8724a457053170bd40c02c204831f
Matilde <majuse@upv.es>
2019-09-06 11:13:18 +0200

    Docker Swarm

--- script/configure.callback.sh
+++ script/configure.callback.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #set bridge.callback.url
-docker exec intermwipsmdeployment_intermw_1 sed -i "s/bridge\.callback\.url\=.*$/bridge\.callback\.url\=$INTERMW_CALLBACK_IP\:$INTERMW_CALLBACK_PORT/g" /etc/inter-iot/intermw/intermw.properties
+docker exec $(docker ps -q -f name=intermw) sed -i "s/bridge\.callback\.url\=.*$/bridge\.callback\.url\=$INTERMW_CALLBACK_IP\:$INTERMW_CALLBACK_PORT/g" /etc/inter-iot/intermw/intermw.properties
 
 #TODO reboot interMW
