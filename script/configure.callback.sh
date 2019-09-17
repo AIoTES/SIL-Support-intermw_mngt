f08eed9d53a16ae219c3004f8a89a5533c2c279c
Matilde <majuse@upv.es>
2019-09-17 11:03:43 +0200

    Change additional configuration properties

--- script/configure.callback.sh
+++ script/configure.callback.sh
@@ -3,4 +3,11 @@
 #set bridge.callback.url
 docker exec $(docker ps -q -f name=intermw) sed -i "s/bridge\.callback\.url\=.*$/bridge\.callback\.url\=$INTERMW_CALLBACK_PROTOCOL\:\/\/$INTERMW_CALLBACK_IP\:$INTERMW_CALLBACK_PORT/g" /etc/inter-iot/intermw/intermw.properties
 
+# Configure other necessary intermw properties
+docker exec $(docker ps -q -f name=intermw) sed -i "s/kafka\.bootstrap\.servers\=.*$/kafka\.bootstrap\.servers\=kafka\:9092/g" /etc/inter-iot/intermw/intermw.properties
+
+docker exec $(docker ps -q -f name=intermw) sed -i "s/kafka\.zookeeper\.connect\=.*$/kafka\.zookeeper\.connect\=zookeeper\:2181/g" /etc/inter-iot/intermw/intermw.properties
+
+docker exec $(docker ps -q -f name=intermw) sed -i "s/ipsm\.api\.baseUrl\=.*$/ipsm\.api\.baseUrl\=http\:\/\/ipsm\:8080/g" /etc/inter-iot/intermw/intermw.properties
+
 #TODO reboot interMW
